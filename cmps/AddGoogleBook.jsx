bf9c604daddafdd53a9cb6c82865100c6a609502
Dorazar <dorazar01@gmail.com>
2025-06-25 23:05:57 +0300

    Add save books from google api

--- cmps/AddGoogleBook.jsx
+++ cmps/AddGoogleBook.jsx
@@ -2,11 +2,65 @@ const { useRef, useEffect, useState, Fragment } = React
 
 const { useParams, useNavigate, Link, Outlet } = ReactRouterDOM
 
+import { bookService } from '../services/book.service.js'
 
 export function AddGoogleBook() {
-    
+  const [searchTerm, setSearchTerm] = useState('')
 
+  const [gData, setGdata] = useState(searchTerm)
 
+  function onSearchBook(searchTerm) {
+    bookService.getGoogledata(searchTerm).then((books) => setGdata(books.items))
+  }
 
-    return (<h1>Im google book</h1>)
+  function handleChange({ target }) {
+    const feild = target.name
+    const value = target.value
+
+    switch (feild.type) {
+      case 'number':
+        value = +value
+        break
+      default:
+        break
+    }
+    setSearchTerm(value)
+  }
+
+  //   const gData =  bookService.getGoogledata().items
+
+  function onBookChoose(book) {
+    bookService.onAddGoogleBook(book).then((book) => bookService.save(book))
+  }
+
+  console.log(gData)
+  return (
+    <section>
+      <section>
+        <h1>Add a google book</h1>
+        <label htmlFor="searchTerm">Book Name</label>
+        <input value={searchTerm} onChange={handleChange} type="text" name="searchTerm" />
+        <button type="button" onClick={() => onSearchBook(searchTerm)}>
+          Search
+        </button>
+      </section>
+
+      {gData && (
+        <ul>
+          {gData.map((book) => (
+            <li key={book.id}>
+              {book.volumeInfo.title}
+              <button
+                onClick={() => {
+                  onBookChoose(book)
+                }}
+              >
+                +
+              </button>
+            </li>
+          ))}
+        </ul>
+      )}
+    </section>
+  )
 }
--- services/async-storage.service.js
+++ services/async-storage.service.js
@@ -31,7 +31,10 @@ function post(entityType, newEntity) {
 
 function put(entityType, updatedEntity) {
     return query(entityType).then(entities => {
-        const idx = entities.findIndex(entity => entity.id === updatedEntity.id)
+        const idx = entities.findIndex((entity) => {
+          entity.id === updatedEntity
+        })
+       
         if (idx < 0) throw new Error(`Update failed, cannot find entity with id: ${entityId} in: ${entityType}`)
         entities.splice(idx, 1, updatedEntity)
         _save(entityType, entities)
