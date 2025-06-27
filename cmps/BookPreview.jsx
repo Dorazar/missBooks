df5c11beac58e09dd8afe837d5c3b6f795446e4e
Dorazar <dorazar01@gmail.com>
2025-06-27 14:51:53 +0300

    Add animate CSS

--- cmps/BookPreview.jsx
+++ cmps/BookPreview.jsx
@@ -1,9 +1,23 @@
+import { utilService } from "../services/util.service.js"
+
+const { useRef, useEffect, useState, Fragment } = React
+
 export function BookPreview({ book }) {
+
+  const imgRef = useRef()
+
+
+
+  function onHover() {
+
+    utilService.animateCSS(imgRef.current,'pulse')
+   
+  }
+
   return (
     <article className="book-preview">
       <h4>{book.title}</h4>
-
-      <img src={book.thumbnail} alt="" />
+      <img onMouseOver={onHover} ref={imgRef} src={book.thumbnail} alt="" />
       {/* {book.listPrice.amount} */}
       {/* {book.listPrice.currencyCode} */}
     </article>
