17a0ed9d822d091d059611d0c6644e42c6d64999
Dorazar <dorazar01@gmail.com>
2025-06-28 13:21:20 +0300

    Add RateByTextBox

--- cmps/RateByTextBox.jsx
+++ cmps/RateByTextBox.jsx
@@ -1,5 +1,23 @@
-export function RateByTextBox({params}) {
-    
+export function RateByTextBox({  onAddVal,onAddReview } ) {
+  
+   function onChooseRating({target}) {
+        const value = target.value
+        const feild = target.name
+        onAddVal(feild,value)
+        console.log(feild,value)
+    }
+  
+    return (
 
-    return (<h1>im by rate by text box</h1>)
+    
+    <section>
+      <h1>Rate by textbox</h1>
+
+        <label htmlFor="rating"></label>
+        <input  onChange={(ev)=>onChooseRating(ev)} type="text" name="rating" placeholder="Enter a rating" />
+        <button onClick={(ev)=>onAddReview(ev)}>Add</button>
+
+
+    </section>
+  )
 }
