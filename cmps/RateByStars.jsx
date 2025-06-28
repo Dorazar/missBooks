fd6898dc1eabd34b0d3fd6e65fa50098e20abf28
Dorazar <dorazar01@gmail.com>
2025-06-28 13:25:45 +0300

    Add RateByStarts

--- cmps/RateByStars.jsx
+++ cmps/RateByStars.jsx
@@ -1,5 +1,30 @@
-export function RateByStars({params}) {
-    
+export function RateByStars({  onAddVal,onAddReview }) {
+  
+       function onChooseRating({target}) {
+        const value = target.value
+        const feild = target.name
+        onAddVal(feild,value)
+        console.log(feild,value)
+    }
+  
+  
+  
+  
+    return (
+    <section>
+      <h1>Rate By Stars</h1>
+           <label htmlFor="rating">Rating</label>
+        <select onChange={(ev)=>onChooseRating(ev)}  name="rating" type="number">
+          <option value="">Select rating</option>
+          <option value="1">⭐</option>
+          <option value="2">⭐⭐</option>
+          <option value="3">⭐⭐⭐</option>
+          <option value="4">⭐⭐⭐⭐</option>
+          <option value="5">⭐⭐⭐⭐⭐</option>
+        </select>
+         <button onClick={(ev)=>onAddReview(ev)}>Add</button>
 
-    return (<h1>im by rate by stars</h1>)
+    </section>
+  )
 }
+
